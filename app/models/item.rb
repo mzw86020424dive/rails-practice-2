a5d3dfb9b8e62633819a3a30b7555d7ca16c3e77
yuta.mizusawa <yuta.mizusawa@PC211NPQ6LT.local>
2022-06-23 17:49:52 +0900

    tagテーブルとモデル、コントローラを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :member
   has_many :favorites
+  has_many :tags
   has_many :favorite_members, through: :favorites, source: :member
 
   def favorited_by?(member)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tags/create'
+  get 'tags/destroy'
   namespace :api do
     namespace :v1 do
       resources :members do
