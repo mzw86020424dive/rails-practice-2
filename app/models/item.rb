8bedc9340020effaa61572dae96f017b27ea0e08
yuta.mizusawa <yuta.mizusawa@PC211NPQ6LT.local>
2022-06-23 17:53:59 +0900

    Revert "tagテーブルとモデル、コントローラを作成"
    This reverts commit a5d3dfb9b8e62633819a3a30b7555d7ca16c3e77.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :member
   has_many :favorites
-  has_many :tags
   has_many :favorite_members, through: :favorites, source: :member
 
   def favorited_by?(member)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'tags/create'
-  get 'tags/destroy'
   namespace :api do
     namespace :v1 do
       resources :members do
